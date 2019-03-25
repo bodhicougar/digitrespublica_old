a116a71c007b3e11d2d444e5607aa043c2b0e217
bodhicougar <contact@blackboxmd.fyi>
2019-03-25 22:15:04 +0100

    Header angepasst

--- config/site.js
+++ config/site.js
@@ -2,7 +2,7 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
   title: 'Die digitale res publica', // Navigation and Site Title
   titleAlt: 'Digitale res publica', // Title for JSONLD
-  description: 'A Gatsby V2 Starter Template Built with a Step By Step Guide',
+  description: 'Digitale res publia: A Gatsby V2 Starter Template Built with a Step By Step Guide',
   url: 'https://digitrespublica.netlify.com', // Domain of your site. No trailing slash!
   siteUrl: 'https://digitrespublica.netlify.com', // url + pathPrefix
   siteLanguage: 'de', // Language Tag on <html> element
@@ -10,7 +10,7 @@ module.exports = {
   banner: 'static/logo/banner.png',
   // JSONLD / Manifest
   favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
-  shortName: 'GatsbyTut', // shortname for manifest. MUST be shorter than 12 characters
+  shortName: 'repGatsby', // shortname for manifest. MUST be shorter than 12 characters
   author: 'bodhicougar', // Author for schemaORGJSONLD
   themeColor: '#3e7bf2',
   backgroundColor: '#d3e0ff',
