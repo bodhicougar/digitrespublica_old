e1c7f5e1e024174314238fea56730c495db3bc2d
bodhicougar <contact@blackboxmd.fyi>
2019-03-25 21:50:53 +0100

    Angepasst!

--- config/site.js
+++ config/site.js
@@ -1,11 +1,11 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: 'Gatsby Tutorial Starter', // Navigation and Site Title
-  titleAlt: 'Gatsby Tutorial Starter', // Title for JSONLD
+  title: 'Die digitale res publica', // Navigation and Site Title
+  titleAlt: 'Digitale res publica', // Title for JSONLD
   description: 'A Gatsby V2 Starter Template Built with a Step By Step Guide',
-  url: 'https://gatsby-tutorial-starter.netlify.com', // Domain of your site. No trailing slash!
-  siteUrl: 'https://gatsby-tutorial-starter.netlify.com', // url + pathPrefix
-  siteLanguage: 'en', // Language Tag on <html> element
+  url: 'https://digitrespublica.netlify.com', // Domain of your site. No trailing slash!
+  siteUrl: 'https://digitrespublica.netlify.com', // url + pathPrefix
+  siteLanguage: 'de', // Language Tag on <html> element
   logo: 'static/logo/logo.png', // Used for SEO
   banner: 'static/logo/banner.png',
   // JSONLD / Manifest
